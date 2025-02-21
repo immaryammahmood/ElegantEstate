c00d5de5966df550505270e2a9aa66efa39ad780
Maryam <bsf23006580@ue.edu.pk>
2025-02-21 11:47:07 +0500

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -19,7 +19,7 @@ export default function Signin() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
+      const res = await fetch('https://elegant-estate.vercel.app/api/auth/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
